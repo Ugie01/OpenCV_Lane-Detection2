be7ac6cfba2694a768156f17a2f8c6388ff78f35
Ugie01 <dlauddnr1@gmail.com>
2025-05-30 14:52:32 +0900

    first commit

--- lane_detect_v2.py
+++ lane_detect_v2.py
@@ -1,6 +1,5 @@
 import cv2 as cv
 import numpy as np
-import math
 
 print(cv.__version__)
 
@@ -33,12 +32,12 @@ def drawLine(rho, theta, img, color, thick):
     cv.line(img, (x1, y1), (x2, y2), color, thick)
     return [x1, y1], [x2, y2]
 
-def classify_environment(img_hls, roi_rect):
+def mode_select(img_hls, roi_rect):
     l_channel = img_hls[:,:,1]
     x, y, w, h = roi_rect
     roi_l = l_channel[y:y+h, x:x+w]
     avg_l = np.mean(roi_l)
-    print(avg_l)
+
     if avg_l < 100:
         return "night"
     elif avg_l >= 150:
@@ -48,28 +47,27 @@ def classify_environment(img_hls, roi_rect):
 
 def dynamic_threshold(img, roi_rect):
     hls = cv.cvtColor(img, cv.COLOR_BGR2HLS)
-    env = classify_environment(hls, roi_rect)
+    env = mode_select(hls, roi_rect)
     print(env)
     if env == "night":
-        return (120, 255), (180, 255), 10
+        return (120, 255), (180, 255), 80
     elif env == "sunny":
-        return (150, 255), (200, 255), 25
+        return (150, 255), (200, 255), 100
     else:  # bright
-        return (180, 255), (220, 255), 20
+        return (180, 255), (220, 255), 120
 
-def combine_ls_channels(img_bgr, l_thresh, y_thresh, sobel_thresh):
+def image_filtering(img_bgr, l_thresh, y_thresh, s_thresh):
     lab = cv.cvtColor(img_bgr, cv.COLOR_BGR2LAB)
     l, a, b = cv.split(lab)
     clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(16,16))
     l_clahe = clahe.apply(l)
     enhanced_lab = cv.merge((l_clahe, a, b))
     enhanced_lab = cv.cvtColor(enhanced_lab, cv.COLOR_LAB2BGR)
-    cv.imshow('enhanced_lab',enhanced_lab)
 
     enhanced_lab = cv.bitwise_and(enhanced_lab, enhanced_lab, mask=roi_mask)
 
     hsv = cv.cvtColor(enhanced_lab, cv.COLOR_BGR2HSV)
-    mask_white = cv.inRange(hsv, (0, 0, 160), (179, 40, 255))
+    mask_white = cv.inRange(hsv, (0, 0, 150), (179, 40, 255))
     mask_yellow = cv.inRange(hsv, (18, 120, 150), (30, 255, 255))
     mask_blue = cv.inRange(hsv, (80, 40, 80), (130, 255, 255))
     combined_mask = cv.bitwise_or(mask_white, mask_yellow)
@@ -82,24 +80,21 @@ def combine_ls_channels(img_bgr, l_thresh, y_thresh, sobel_thresh):
     lab = cv.cvtColor(enhanced_lab, cv.COLOR_BGR2YUV)
     l_channel = lab[:,:,0]
     _, lab_binary = cv.threshold(l_channel, l_thresh[0], 255, cv.THRESH_BINARY)
-    # cv.imshow('lab_binary',lab_binary)
 
     # YUV 색공간 (도로 환경에 효과적)
     yuv = cv.cvtColor(enhanced_lab, cv.COLOR_BGR2YUV)
     y_channel = yuv[:,:,0]
     _, yuv_binary = cv.threshold(y_channel, y_thresh[0], 255, cv.THRESH_BINARY)
-    # cv.imshow('yuv_binary', yuv_binary)
 
     # 가중 결합
-    combined = cv.bitwise_and(lab_binary, yuv_binary)  # AND로 노이즈 감소
-    combined = cv.bitwise_or(combined, masked_gray)    # 색상 마스크 추가
+    combined = cv.bitwise_and(lab_binary, yuv_binary)
+    combined = cv.bitwise_or(combined, masked_gray)
     
-    # Sobel 최적화 (CV_16S, ksize=5)
+    # Sobel 최적화
     sobelx = cv.Sobel(combined, cv.CV_64F, 1, 0, ksize=3)
     abs_sobel = cv.convertScaleAbs(sobelx)
-    _, sobel_binary = cv.threshold(abs_sobel, 100, 255, cv.THRESH_BINARY)
-    # combined = cv.bitwise_or(combined, sobel_binary)
-    cv.imshow('combined1', sobel_binary)
+    _, sobel_binary = cv.threshold(abs_sobel, s_thresh, 255, cv.THRESH_BINARY)
+    cv.imshow('combined', sobel_binary)
 
     return combined
 
@@ -116,16 +111,17 @@ while cap.isOpened():
 
     # 2. ROI 설정 (하단 사다리꼴)
     h, w = frame.shape[:2]
-    print(h,w)
     roi_pts = np.array([
-        [int(w*0.4), int(h*0.6)],
-        [int(w*0.0), int(h*0.9)],
-        [int(w*1.0), int(h*0.9)],
-        [int(w*0.6), int(h*0.6)],
-        [int(w*0.5), int(h*0.6)],
-        [int(w*0.75), int(h*0.85)],
-        [int(w*0.25), int(h*0.85)],
-        [int(w*0.5), int(h*0.6)],
+        [int(w*0.44), int(h*0.6)],
+        [int(w*0.0), int(h*0.85)],
+        [int(w*0.0), int(h*0.95)],
+        [int(w*1.0), int(h*0.95)],
+        [int(w*1.0), int(h*0.85)],
+        [int(w*0.56), int(h*0.6)],
+        [int(w*0.52), int(h*0.6)],
+        [int(w*0.78), int(h*0.95)],
+        [int(w*0.22), int(h*0.95)],
+        [int(w*0.48), int(h*0.6)],
     ], dtype=np.int32)
 
     syROI=roi_pts[0][1]
@@ -135,19 +131,18 @@ while cap.isOpened():
     cv.fillPoly(roi_mask, [roi_pts], 255)
     x, y, w_roi, h_roi = cv.boundingRect(roi_pts)
     roi_rect = (x, y, w_roi, h_roi)
+    cv.imshow('roi_mask',roi_mask)
 
     # 3. 환경별 임계값 계산
     l_thresh, y_thresh, sobel_thresh = dynamic_threshold(frame, roi_rect)
 
     # 4. 차선 후보 이진화
-    # frame = cv.medianBlur(frame, 5)
-    lane_binary = combine_ls_channels(frame, l_thresh, y_thresh, sobel_thresh)
+    lane_binary = image_filtering(frame, l_thresh, y_thresh, sobel_thresh)
     lane_binary = cv.bitwise_and(lane_binary, roi_mask)
 
     # 5. 엣지 검출 & 허프 라인
     edges = cv.Canny(lane_binary, 50, 200)
     lines = cv.HoughLines(edges, 1, np.pi/180, 30)
-    # cv.imshow('edges',edges)
     imgHough = frame.copy()
 
     # 6. 직선 평균화 및 시각화
@@ -179,22 +174,11 @@ while cap.isOpened():
             p1, p2 = drawLine(avg_rho, avg_theta, imgHough, (0,0,255), 2)
             pt1[i], pt2[i] = p1, p2
             # Detecting the lane
-            retval, pt1[i], pt2[i] = cv.clipLine((roi_pts[1][0], syROI, roi_pts[2][0], eyROI-syROI), p1, p2)
-            cv.rectangle(imgHough, [roi_pts[1][0], syROI], [roi_pts[2][0], eyROI], (0, 128, 0), 2)
+            retval, pt1[i], pt2[i] = cv.clipLine((roi_pts[1][0], syROI, roi_pts[3][0], eyROI-syROI), p1, p2)
+            cv.rectangle(imgHough, [roi_pts[1][0], syROI], [roi_pts[3][0], eyROI], (0, 128, 0), 2)
             old_thetaQ[i]=thetaQ[i]
             old_rhoQ[i]=rhoQ[i]
             old_countQ[i]=countQ[i]
-
-    # roi_pts = np.array([
-    #     [int(w*0.3), int(h*0.6)],
-    #     [int(w*0.05), int(h*0.9)],
-    #     [int(w*0.95), int(h*0.9)],
-    #     [int(w*0.7), int(h*0.6)],
-    #     # [int(w*0.55), int(h*0.5)],
-    #     # [int(w*0.8), int(h*0.85)],
-    #     # [int(w*0.3), int(h*0.85)],
-    #     # [int(w*0.5), int(h*0.5)],
-
 
     # 7. 차선 영역 시각화
     imgOut = np.zeros_like(frame)
@@ -206,10 +190,10 @@ while cap.isOpened():
 
     # 8. Bird's Eye View 변환
     pts1 = np.float32([
-        [int(w*0.35), h],
-        [int(w*0.25), int(h*0.85)],
-        [int(w*0.75), int(h*0.85)],
-        [int(w*0.65), h]
+        [int(w*0.0), int(h*0.95)],
+        [int(w*0.3), int(h*0.6)],
+        [int(w*0.7), int(h*0.6)],
+        [int(w*1.0), int(h*0.95)],
     ])
     pts2 = np.float32([
         [0, 540], [0, 0], [960, 0], [960, 540]
@@ -219,7 +203,7 @@ while cap.isOpened():
 
     # 9. 결과 출력
     cv.imshow('Overlap Lane', overlapImage)
-    cv.imshow('Hough Lines', imgHough)
+    cv.imshow('perspectiveImg', perspectiveImg)
     out.write(overlapImage)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
